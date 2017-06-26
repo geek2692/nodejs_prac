86da515677395690ed74f2248b3309f2cb97b3d7
geek2692 <geek2692@gmail.com>
2017-06-26 16:05:18 +0900

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL
+|| 'postgres://postgres:1234@localhost:5432/todo';
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
